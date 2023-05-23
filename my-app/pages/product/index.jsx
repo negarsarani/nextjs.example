995422104da1acb13704b17fcfde0adc44b75b28
Negar Sarani <negar.sarani2000@gmail.com>
2023-05-23 11:55:01 +0330

    Navigating-Programmatically

--- my-app/pages/product/index.jsx
+++ my-app/pages/product/index.jsx
@@ -10,7 +10,7 @@ const Product = () => {
       <Link href={'/product/1'}>p1</Link>
 
       <h3 onClick={() => handleRouter(2)}>P2</h3>
-      <h3 onClick={() => handleRouter(3)}>P3</h3>
+      <h3 onClick={() => handleRouter(4)}>P3</h3>
     </div>
   );
 };
