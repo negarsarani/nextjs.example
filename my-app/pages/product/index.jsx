387a55658ba5d3ce0b84a03d8bf51133cbf74e82
Negar Sarani <negar.sarani2000@gmail.com>
2023-05-23 11:45:11 +0330

    Link-Component-Navigation

--- my-app/pages/product/index.jsx
+++ my-app/pages/product/index.jsx
@@ -1,12 +1,13 @@
-
+import Link from 'next/link';
 const Product = () => {
-    return (
-        <div>
-            <h3>P1</h3>
-            <h3>P2</h3>
-            <h3>P3</h3>
-        </div>
-    );
+  return (
+    <div>
+      <Link href={'/product/1'}>p1</Link>
+
+      <h3>P2</h3>
+      <h3>P3</h3>
+    </div>
+  );
 };
 
 export default Product;
